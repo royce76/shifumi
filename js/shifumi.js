99045151c1ea413c3d9b0573643ca1425a8e954c
royce76 <67342235+royce76@users.noreply.github.com>
2020-07-02 15:21:12 +0200

    add name

--- js/shifumi.js
+++ js/shifumi.js
@@ -1,4 +1,8 @@
 // Display "Welcome " to user
 alert("Welcome to Advanced SHIFUMI :\npress ok to continue");
 
+//User add his name
+var userName=prompt("Please enter your name\nand press ok to continue");
+console.log(userName);
+//variable userName store user name.
 
