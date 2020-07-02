35a9b0b284e12d4f49951d1c04356784d2afa923
royce76 <67342235+royce76@users.noreply.github.com>
2020-07-02 16:02:44 +0200

    weapon's choice

--- js/shifumi.js
+++ js/shifumi.js
@@ -6,3 +6,15 @@ var userName=prompt("Please enter your name\nand press ok to continue");
 console.log(userName);
 //variable userName store user name.
 
+//add weapon's user
+var weapon=[
+    "stone",
+    "leaf",
+    "scissors"
+];
+
+var choiceUser=prompt( "Please choose and enter name's weapon among this list : "
+                        +weapon +"\n and press ok to continue"
+                    );
+console.log(choiceUser);
+//variable choiceUser store weapon's choice.
