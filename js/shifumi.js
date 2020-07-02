3081a8c1b929386fee6df265b95fc8af9d5e48e6
royce76 <67342235+royce76@users.noreply.github.com>
2020-07-02 16:38:20 +0200

    computer choice

--- js/shifumi.js
+++ js/shifumi.js
@@ -18,3 +18,27 @@ var choiceUser=prompt( "Please choose and enter name's weapon among this list : 
                     );
 console.log(choiceUser);
 //variable choiceUser store weapon's choice.
+
+//show user choice
+alert("You choose "+choiceUser+"\n press ok to continue");
+
+//generate a choice for computer
+//variable i has a random value
+var i=Math.random();
+console.log(i);
+
+//computer choose stone for i<0.33
+if(i<=0.33){
+    alert("Computer choose "+ weapon[0] + "\n press ok to continue");
+    console.log(weapon[0]);
+}
+//computer choose leaf for i<0.66
+else if (i<=0.66){
+    alert("Computer choose "+ weapon[1] + "\n press ok to continue");
+    console.log(weapon[1]);
+}
+//else computer choose scissors 
+else{
+    alert("Computer choose "+ weapon[2]+ "\n press ok to continue");
+    console.log(weapon[2]);
+}
