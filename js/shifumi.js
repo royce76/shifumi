f0c6c129ddf97dab9c941cc67e718f3a71ca5440
royce76 <67342235+royce76@users.noreply.github.com>
2020-07-05 14:34:27 +0200

    oragnize the code

--- js/shifumi.js
+++ js/shifumi.js
@@ -1,80 +1,84 @@
-alert("Welcome to Advanced SHIFUMI :\npress ok to continue");// Display "Welcome " to user
+alert("Welcome to Advanced SHIFUMI :\n3 points to win\npress ok to continue");// Display "Welcome " to user
 
 do{
-var userName = prompt("Please enter your name\nand press ok to continue");//variable userName store user name.
+   var userName = prompt("Please enter your name\nand press ok to continue");//variable userName store user name.
 }
 while(userName.length > 20 || userName.length < 2);
 do{
-var pointComputer = 0;
-var pointUser = 0 ;
+    var pointComputer = 0;
+    var pointUser = 0 ;
 
-while(pointComputer < 3 && pointUser < 3){
+    while(pointComputer < 3 && pointUser < 3){
+                                              alert("Computer has " + pointComputer + ".\n" + userName + " has " + pointUser +".")
     
-var weapon = ["roc", "paper", "scissors"];//add weapon's user
+                                              var weapon = ["roc", "paper", "scissors"];//add weapon's user
 
-do{
-    var choiceUser = prompt(
-                        "Please choose and enter name's weapon among this list :\n" //variable choiceUser store weapon's choice.
-                        + weapon[0] + "\n" + weapon[1] + "\n" + weapon[2] + 
-                        ".\nand press ok to continue"
-                    );
-                    choiceUser = choiceUser.toLowerCase();
-}
-while (
-       choiceUser != "roc" && choiceUser != "paper" && choiceUser != "scissors"  
-);
+                                                do{
+                                                    var choiceUser = prompt(
+                                                                            "Please choose and enter name's weapon among this list :\n" //variable choiceUser store weapon's choice.
+                                                                             + weapon[0] + "\n" + weapon[1] + "\n" + weapon[2] + 
+                                                                            ".\nand press ok to continue"
+                                                                    );
+                                                                        choiceUser = choiceUser.toLowerCase();
+                                                }
+                                                while (
+                                                       choiceUser != "roc" && choiceUser != "paper" && choiceUser != "scissors"  
+                                                );
 
-var i = Math.random();//generate a choice for computerS
+                                                    var i = Math.random();//generate a choice for computerS
 
-var computerChoice = "";//var computerChoice store the weapon value
+                                                    var computerChoice = "";//var computerChoice store the weapon value
 
-if(i <= 0.33){
-              alert("Computer choose "+ weapon[0] + "\npress ok to continue");//computer choose stone for i<0.33
-              computerChoice = weapon[0];
-}
-else if (i <= 0.66){
-                    alert("Computer choose "+ weapon[1] + "\npress ok to continue");//computer choose leaf for i<0.66
-                    computerChoice = weapon[1];
-}
-else{
-     alert("Computer choose "+ weapon[2]+ "\npress ok to continue");//else computer choose scissors
-     computerChoice = weapon[2];
-}
+                                                    if(i <= 0.33){
+                                                                  alert("Computer choose "+ weapon[0] + "\npress ok to continue");//computer choose stone for i<0.33
+                                                                  computerChoice = weapon[0];
+                                                    }
+                                                    else if (i <= 0.66){
+                                                                        alert("Computer choose "+ weapon[1] + "\npress ok to continue");//computer choose leaf for i<0.66
+                                                                        computerChoice = weapon[1];
+                                                    }
+                                                    else{
+                                                         alert("Computer choose "+ weapon[2]+ "\npress ok to continue");//else computer choose scissors
+                                                         computerChoice = weapon[2];
+                                                    }
 
-alert("Computer has chosen "+ computerChoice + ".\n" + userName + " has chosen " + choiceUser + ".\npress ok to continue");//display user choice and computer choice
+                                                    alert("Computer has chosen "+ computerChoice + ".\n" + userName + " has chosen " + choiceUser + ".\npress ok to continue");//display user choice and computer choice
 
-//determinate winner
+                                                    //determinate winner
 
 
-if(choiceUser === computerChoice){
-                                 var y = alert("DRAW");
-}
-else if((choiceUser === weapon[0] && computerChoice === weapon[2]) ||
-        (choiceUser === weapon[1] && computerChoice === weapon[0]) ||
-        (choiceUser === weapon[2] && computerChoice === weapon[1])
-       ){
-         alert(userName + " win. ");
-         pointUser ++;
-}
-else{
-     alert("Computer win");
-     pointComputer ++;
-}
-}
-if(pointUser === 3){
-    alert(userName + " win the set.");
-}
-else{
-alert("Computer win the set");
-}
-do{
-var anwserUser = prompt('Do you want to play an another set ?\nAnswer "yes" to continue \n"no" to stop');
-    anwserUser = anwserUser.toLocaleLowerCase();
-}
-while(anwserUser != "no" && anwserUser != "yes");
+                                                    if(choiceUser === computerChoice){
+                                                                                      var y = alert("DRAW");
+                                                    }
+                                                    else if((choiceUser === weapon[0] && computerChoice === weapon[2]) ||
+                                                            (choiceUser === weapon[1] && computerChoice === weapon[0]) ||
+                                                            (choiceUser === weapon[2] && computerChoice === weapon[1])
+                                                    ){
+                                                     alert(userName + " win. ");
+                                                     pointUser ++;
+                                                    }
+                                                    else{
+                                                         alert("Computer win");
+                                                         pointComputer ++;
+                                                    }
+    }
+
+    if(pointUser === 3){
+                        alert(userName + " win the set.");
+    }
+    else{
+         alert("Computer win the set");
+    }
+
+    do{
+        var anwserUser = prompt('Do you want to play an another set ?\n- " yes " to continue \n- " no " to stop');
+        anwserUser = anwserUser.toLocaleLowerCase();
+    }
+    while(anwserUser != "no" && anwserUser != "yes");
 
 }
 while( anwserUser === "yes");
+
 if(anwserUser === "no"){
-    alert(" Thank you for playing my game");
+                        alert(" Thank you for playing my game");
 }
